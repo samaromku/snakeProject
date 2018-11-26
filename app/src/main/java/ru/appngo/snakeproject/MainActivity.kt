ec7ba7450fb96c9688ada196bf0e50a1e6969469
aasavchenko <andrey.savchenko@tala.co>
2018-11-26 19:26:45 +0300

    first commit

---
+++ app/src/main/java/ru/appngo/snakeproject/MainActivity.kt
@@ -0,0 +1,35 @@
+package ru.appngo.snakeproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val head = View(this)
+        head.layoutParams = LinearLayout.LayoutParams(100, 100)
+        head.background = ContextCompat.getDrawable(this, R.drawable.circle)
+
+
+        Thread(Runnable {
+            while (true) {
+                Thread.sleep(500)
+                runOnUiThread{
+                    (head.layoutParams as LinearLayout.LayoutParams).topMargin += 100
+                    container.removeView(head)
+                    container.addView(head)
+                }
+            }
+        }).start()
+
+
+
+    }
+}
